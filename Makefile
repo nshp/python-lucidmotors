29f00f92d3c060f760f4c833972e9171de20be54
Nick Shipp <nick@shipp.ninja>
2023-11-23 18:17:38 -0500

    Add some gRPC debugging code, fill in protobuf types for login response

---
+++ Makefile
@@ -0,0 +1,71 @@
+PYTHON=python
+
+PROTO_DIR=proto
+GEN_DIR=lucidmotors/gen
+
+
+ifeq ($(V),1)
+Q=
+else
+Q=@
+endif
+
+
+.PHONY: usage
+usage:
+	@echo "Usage:"
+	@echo "  make requirements  - Regenerate requirements.txt files with Poetry"
+	@echo "  make protobuf      - Compile gRPC/ProtoBuf definitions"
+	@echo "  make clean         - Clean build artifacts"
+
+
+#
+# Generate requirements files for pip from pyproject.toml
+#
+
+requirements.txt: pyproject.toml
+	@echo EXPORT $@
+	$(Q)$(PYTHON) -m poetry export -o requirements.txt --without-hashes
+
+requirements_dev.txt: pyproject.toml
+	@echo EXPORT $@
+	$(Q)$(PYTHON) -m poetry export -o requirements_dev.txt --without-hashes --with dev
+
+.PHONY: requirements
+requirements: requirements.txt requirements_dev.txt
+
+
+#
+# Compile gRPC/ProtoBuf definitions
+#
+
+PROTOS= login_session.proto        \
+	user_profile_service.proto \
+	vehicle_state_service.proto
+
+PROTOS_GEN := $(patsubst %.proto,%_pb2.py,$(PROTOS))      \
+	      $(patsubst %.proto,%_pb2_grpc.py,$(PROTOS)) \
+	      $(patsubst %.proto,%_pb2.pyi,$(PROTOS))
+
+PROTOS := $(addprefix $(PROTO_DIR)/,$(PROTOS))
+PROTOS_GEN := $(addprefix $(GEN_DIR)/,$(PROTOS_GEN))
+
+# We have some sed insanity here because protoc doesn't support generating relative imports
+$(GEN_DIR)/%_pb2.py $(GEN_DIR)/%_pb2_grpc.py $(GEN_DIR)/%_pb2.pyi: $(PROTO_DIR)/%.proto
+	@echo PROTOC $*
+	$(Q)mkdir -p $(GEN_DIR)
+	$(Q)touch $(GEN_DIR)/__init__.py
+	$(Q)$(PYTHON) -m grpc_tools.protoc -I $(PROTO_DIR) --python_out=$(GEN_DIR) --pyi_out=$(GEN_DIR) --grpc_python_out=$(GEN_DIR) $<
+	$(Q)sed -i 's/^import \(\w\+_pb2\)\b/from . import \1/' $(GEN_DIR)/$*_pb2.py $(GEN_DIR)/$*_pb2_grpc.py $(GEN_DIR)/$*_pb2.pyi
+
+.PHONY: protobuf
+protobuf: $(PROTOS_GEN)
+
+.PHONY: clean-protos
+clean-protos:
+	$(Q)rm -rf $(GEN_DIR)
+
+
+
+.PHONY: clean
+clean: clean-protos
--- grpc_fiddle.py
+++ grpc_fiddle.py
@@ -1,12 +1,60 @@
+from typing import Any
+from google.protobuf.unknown_fields import UnknownFieldSet
+
+import uuid
+import grpc
+import ipdb
 import logging
 import getpass
+import google._upb
+import google.protobuf
 
-import grpc
-from grpc_reflection.v1alpha.proto_reflection_descriptor_database import (
-    ProtoReflectionDescriptorDatabase,
-)
-import login_session_pb2
-import login_session_pb2_grpc
+from lucidmotors.gen import login_session_pb2
+from lucidmotors.gen import login_session_pb2_grpc
+
+wire_types = {
+    0: 'varint',
+    1: 'fixed-64bit',
+    2: 'length-delimited',
+    3: 'group-start',
+    4: 'group-end',
+    5: 'fixed-32bit',
+}
+
+def message_dump_recursive(message: Any, depth: int = 0):
+    if isinstance(message, (google._upb._message.RepeatedScalarContainer, google._upb._message.RepeatedCompositeContainer)):
+        for elem in message:
+            message_dump_recursive(elem, depth=depth)
+        return
+
+    if not isinstance(message, google.protobuf.message.Message):
+        return
+
+    indent = ' ' * depth
+    print(f'{indent}{type(message)}:')
+
+    depth += 1
+    indent = ' ' * depth
+
+    for field in UnknownFieldSet(message):
+        wire_type = wire_types[field.wire_type]
+        print(f'{indent}Unknown field {field.field_number} wire type {wire_type}: {field.data!r}')
+
+    for descriptor, field in message.ListFields():
+        if isinstance(field, (google.protobuf.message.Message, google._upb._message.RepeatedScalarContainer, google._upb._message.RepeatedCompositeContainer)):
+            field_desc_short = ''
+        elif descriptor.enum_type is not None:
+            # TODO: Handle a list of enum, like LoginResponse.subscriptions
+            enum = descriptor.enum_type
+            if field in enum.values_by_number:
+                name = enum.values_by_number[field].name
+                field_desc_short = f'{name} ({field})'
+            else:
+                field_desc_short = f'UNKNOWN ENUMERATOR: {field}'
+        else:
+            field_desc_short = str(field)
+        print(f'{indent}Field {descriptor.number}, {descriptor.name}: {field_desc_short}')
+        message_dump_recursive(field, depth=depth)
 
 
 def main():
@@ -15,16 +63,19 @@ def main():
 
     with grpc.secure_channel("mobile.deneb.prod.infotainment.pdx.atieva.com", grpc.ssl_channel_credentials()) as channel:
         stub = login_session_pb2_grpc.LoginSessionStub(channel)
+        device_id = f'{uuid.getnode():x}'
         req = login_session_pb2.LoginRequest(
             username=username,
             password=password,
             notification_channel_type=login_session_pb2.NotificationChannelType.NOTIFICATION_CHANNEL_ONE,
+            notification_device_token=device_id,
             os=login_session_pb2.Os.OS_IOS,
             locale='en_US',
-            device_id='python-lucidmotors',
+            client_name='python-lucidmotors',
+            device_id=device_id,
         )
         response = stub.Login(req)
-        print(response)
+        message_dump_recursive(response)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
