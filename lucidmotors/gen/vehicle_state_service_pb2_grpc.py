060bdf8ee84909deefafdad41855a426102bf48e
Nick Shipp <nick@shipp.ninja>
2025-05-08 11:54:11 -0400

    Update deps and rebuild protobuf

--- lucidmotors/gen/vehicle_state_service_pb2_grpc.py
+++ lucidmotors/gen/vehicle_state_service_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from . import vehicle_state_service_pb2 as vehicle__state__service__pb2
 
-GRPC_GENERATED_VERSION = '1.71.0'
+GRPC_GENERATED_VERSION = '1.67.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
